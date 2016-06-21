902a6f54b557bdc3882dfafc274883530a2e1531
Cli98 <hdbjlcl@163.com>
2016-06-21 09:08:12 -0500

    Update file.py
    Sorry but there is something wrong in my calculation and code.
    
    I have updated it correctly.
    
    Help me to check thx

--- member/lcl/file.py
+++ member/lcl/file.py
@@ -1,119 +1,110 @@
-# coding: utf-8 
-3 
- 
-4 # In[26]: 
-5 
- 
-6 # check missing value function 
-7 def missingcheck(obj): 
-8     flag="No missing!" 
-9     for bool in obj: 
-10         if bool==True: 
-11           flag="missing entry detected!"   
-12     print(flag)   
-13 
- 
-14 
- 
-15 # In[2]: 
-16 
- 
-17 # step 2: modify dataframe 
-18 se=pd.Series(ds["uid"]) 
-19 ds.index=se 
-20 ds=ds.drop("uid",axis=1) 
-21 print(ds) 
-22 
- 
-23 
- 
-24 # In[2]: 
-25 
- 
-26 ##step 1 : import original data 
-27 import pandas as pd 
-28 import json as js 
-29 
- 
-30 path = r"C:\Users\Changlin Li\Desktop\user.json\user.json" 
-31 record=[js.loads(line) for line in open(path)] 
-32 ds=pd.DataFrame(record) 
-33 print(ds) 
-34 
- 
-35 
- 
-36 # In[35]: 
-37 
- 
-38 # step 3: check null values 
-39 counter=pd.isnull(ds) 
-40 missingcheck(counter["app"])  
-41 missingcheck(counter["appfile"]) 
-42 missingcheck(counter["activity_time"]) 
-43 missingcheck(counter["label"]) 
-44 missingcheck(counter["category_id"]) 
-45     
-46 
- 
-47 
- 
-48 # In[20]: 
-49 
- 
-50 # step 4 check duplicate UID 
-51 ds1=pd.DataFrame(ds.index) 
-52 booleans=ds1.duplicated() 
-53 flag="No duplicated!" 
-54 for bool in booleans: 
-55     if bool==True: 
-56       flag="duplicate entry detected!"   
-57 print(flag) 
-58 
- 
-59 
- 
-60 # In[3]: 
-61 
- 
-62 # step 5 sort dataframe accroding to uid 
-63 ds=ds.sort_index(axis=0) 
-64 print(ds) 
-65 
- 
-66 
- 
-67 # In[17]: 
-68 
- 
-69 # step 6 sort dataframe accroding to activity_time 
-70 ds=ds.sort_values(by="activity_time") 
-71 print(ds) 
-72 
- 
-73 
- 
-74 # In[16]: 
-75 
- 
-76 # step 7, sub-step 1： get total entries, training sets , 
-77 ds_sample=ds[:300].label 
-78 print(len(ds_sample)) 
-79 print(ds_sample.describe())
+# coding: utf-8
 
-# compute the weight of non-empty category_id user in all VIP-purchase group
-length=len(ds)
-ds=ds[ds>0]
-ds=ds.dropna()                     
-# clear na value
-print(len(ds))
+# In[26]:
+
+# check missing value function, needed in step 3
+def missingcheck(obj):
+    flag="No missing!"
+    for bool in obj:
+        if bool==True:
+          flag="missing entry detected!"  
+    print(flag)  
+
+
+# In[2]:
+
+# step 2: modify dataframe
+se=pd.Series(ds["uid"])
+ds.index=se
+ds=ds.drop("uid",axis=1)
 print(ds)
-print(ds.describe())
-ds.sort_values(by="category_id")
-ds=ds.loc[ds["category_id"]!="[]"]
-counter=len(ds)
-appPerc=counter/length
+
+
+# In[1]:
+
+##step 1 : import original data
+import pandas as pd
+import json as js
+
+path = r"C:\Users\Changlin Li\Desktop\user.json\user.json"
+record=[js.loads(line) for line in open(path)]
+ds=pd.DataFrame(record)
+print(ds)
+
+
+# In[34]:
+
+# step 3: check null values, use fun in the beginning
+counter=pd.isnull(ds)
+missingcheck(counter["app"]) 
+missingcheck(counter["appfile"])
+missingcheck(counter["activity_time"])
+missingcheck(counter["label"])
+missingcheck(counter["category_id"])
+   
+
+
+# In[35]:
+
+# step 4 check duplicate UID
+ds1=pd.DataFrame(ds.index)
+booleans=ds1.duplicated()
+flag="No duplicated!"
+for bool in booleans:
+    if bool==True:
+      flag="duplicate entry detected!"  
+print(flag)
+
+
+# In[3]:
+
+# step 5 sort dataframe accroding to uid
+ds=ds.sort_index(axis=0)
+print(ds)
+
+
+# In[17]:
+
+# step 6 sort dataframe accroding to activity_time
+ds=ds.sort_values(by="activity_time")
+print(ds)
+
+
+# In[6]:
+
+# step 7, sub-step 1： get total entries, training sets , small sample test(300 units)
+ds_sample=ds[:300]
+ds_sample=ds_sample[ds_sample["label"]>0]
+ds_sample=ds_sample.dropna() 
+length=len(ds_sample)
+# clear na value
+ds_sample.sort_values(by="category_id")
+count1=0
+print(ds_sample["category_id"])
+for ele in ds_sample["category_id"]:
+    if ele==[]:
+        count1+=1
+print(count1)
+appPerc=count1/length
 print(appPerc)
- 
+
+
+# In[4]:
+
+# sub-step2: compute the weight of non-empty category_id user in all VIP-purchase group
+ds=ds[ds["label"]>0]
+ds=ds.dropna()  
+# clear na value
+length=len(ds) 
+ds=ds.sort_values(by="category_id")
+count1=0
+for ele in ds["category_id"]:
+    if ele==[]:
+        count1+=1
+appPerc=count1/length
+print(appPerc)
+# conclusion: there is no possibility to remove empty category_id users.
+
+
 
  
